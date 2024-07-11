c796a3f4a55abe8d466359e246dc8a0b0d598fdb
Rain Tüür <rain.tyyr@bcs.ee>
2024-07-11 09:50:37 +0300

    sprint 3 tests

--- src/test/java/ee/bcs/carportal/controller/CarControllerMvcTest.java
+++ src/test/java/ee/bcs/carportal/controller/CarControllerMvcTest.java
@@ -21,15 +21,39 @@ class CarControllerMvcTest {
         mockMvc.perform(get("/api/v1/cars/all"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("text/plain;charset=UTF-8"))
-                .andExpect(content().string("All car models: Model 3, Civic, Camry, F-150, Prius;"));
+                .andExpect(content().string("All car models: Model 3, Civic, Camry, F-150, Prius; (number of car models: 5)"));
     }
 
     @Test
-    public void shouldReturn28000To44000() throws Exception {
+    public void shouldReturn3Models() throws Exception {
         mockMvc.perform(get("/api/v1/cars/price-range/from/to")) // Assuming the URL parameters are included in the path
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("text/plain;charset=UTF-8"))
-                .andExpect(content().string("from €28000 to €44000"));
+                .andExpect(content().string("Cars in price range €28000 - €44000: Model 3, Camry, Prius; (number of car models: 3)"));
+    }
+
+    @Test
+    public void shouldReturn2GreenModels() throws Exception {
+        mockMvc.perform(get("/api/v1/cars/green/price-range/from/to")) // Assuming the URL parameters are included in the path
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string("Green cars in price range €28000 - €44000: Model 3 (Electric), Prius (Hybrid); (number of car models: 2)"));
+    }
+
+    @Test
+    public void shouldReturnCorrectTeslaModel3RegistrationTaxRateAndAmount() throws Exception {
+        mockMvc.perform(get("/api/v1/car/id/registration-tax")) // Assuming the URL parameters are included in the path
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string("The registration tax rate for 2020 Tesla Model 3 is 2.5% with total tax amount €1100"));
+    }
+
+    @Test
+    public void shouldReturnCorrectTeslaModel3AnnualTaxFee() throws Exception {
+        mockMvc.perform(get("/api/v1/car/id/annual-tax")) // Assuming the URL parameters are included in the path
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string("The annual tax for 2020 Tesla Model 3 is €50"));
     }
 
 }
