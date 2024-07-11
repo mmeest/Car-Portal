9e5f8506bc98e914379cd9a9d3c1e586c2ecb17e
Rain Tüür <rain.tyyr@bcs.ee>
2024-07-11 09:45:58 +0300

    sprint 2 solution

--- src/main/java/ee/bcs/carportal/controller/CarController.java
+++ src/main/java/ee/bcs/carportal/controller/CarController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CarController {
 
+    private final String[] carModels = {"Model 3", "Civic", "Camry", "F-150", "Prius"};
+    private final String[] manufacturers = {"Tesla", "Honda", "Toyota", "Ford", "Toyota"};
+    private final int[] modelYears = {2020, 2021, 2022, 2023, 2020};
+    private final String[] fuelTypes = {"Electric", "Petrol", "Petrol", "Petrol", "Hybrid"};
+    private final double[] emissions = {0.0, 0.05, 0.04, 0.1, 0.03};
+    private final int[] prices = {44000, 25000, 28000, 45000, 30000};
 
     // Mandatory endpoints go to below
     // Please use @Tag annotation as below with all mandatory endpoints:
@@ -17,15 +23,24 @@ public class CarController {
     @GetMapping("/cars/all")
     @Tag(name = "Mandatory")
     public String getAllCars() {
-
-        return "to be implemented";
+        StringBuilder sb = new StringBuilder("All car models: ");
+        for (String model : carModels) {
+            sb.append(model).append(", ");
+        }
+        return sb.substring(0, sb.length() - 2) + ";";
     }
 
+    @GetMapping("/cars/price-range/from/to")
+    @Tag(name = "Mandatory")
+    public String getCarsInPriceRange() {
+        int from = 28000;
+        int to = 44000;
+        return "from €" + from + " to €" + to;
+    }
 
     // Extra practice endpoints go to below
     // Please use @Tag annotation as below with all extra practice endpoints:
     // @Tag(name = "Extra practice")
-
 
     // ALL PRIVATE METHODS go to below
 
