e8ab219b8c53b990bba418f0b7757a225ad98ced
Rain Tüür <rain.tyyr@bcs.ee>
2024-07-11 09:49:14 +0300

    sprint 2 tests

--- src/test/java/ee/bcs/carportal/controller/CarControllerMvcTest.java
+++ src/test/java/ee/bcs/carportal/controller/CarControllerMvcTest.java
@@ -1,8 +1,13 @@
 package ee.bcs.carportal.controller;
 
 import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(CarController.class)
@@ -11,5 +16,20 @@ class CarControllerMvcTest {
     @Resource
     private MockMvc mockMvc;
 
+    @Test
+    public void shouldReturnAllModels() throws Exception {
+        mockMvc.perform(get("/api/v1/cars/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string("All car models: Model 3, Civic, Camry, F-150, Prius;"));
+    }
+
+    @Test
+    public void shouldReturn28000To44000() throws Exception {
+        mockMvc.perform(get("/api/v1/cars/price-range/from/to")) // Assuming the URL parameters are included in the path
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string("from €28000 to €44000"));
+    }
 
 }
