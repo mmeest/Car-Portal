d957e7954ebb7339b82b8c7a02d70f3a838b2f65
Rain Tüür <rain.tyyr@bcs.ee>
2025-01-06 10:15:37 +0200

    sprint 6 CP-32

--- src/main/java/ee/bcs/carportal/persistence/Car.java
+++ src/main/java/ee/bcs/carportal/persistence/Car.java
@@ -3,6 +3,7 @@ package ee.bcs.carportal.persistence;
 import java.util.Objects;
 
 public class Car {
+    private Integer id;
     private String carModel;
     private String manufacturer;
     private int modelYear;
@@ -23,7 +24,24 @@ public class Car {
         this.price = price;
     }
 
-    // Getters and Setters
+    public Car(Integer id, String carModel, String manufacturer, int modelYear, FuelType fuelType, double emissions, int price) {
+        this.id = id;
+        this.carModel = carModel;
+        this.manufacturer = manufacturer;
+        this.modelYear = modelYear;
+        this.fuelType = fuelType;
+        this.emissions = emissions;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getCarModel() {
         return carModel;
     }
@@ -77,21 +95,25 @@ public class Car {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Car car = (Car) o;
-        return modelYear == car.modelYear && Double.compare(car.emissions, emissions) == 0 && price == car.price && carModel.equals(car.carModel) && manufacturer.equals(car.manufacturer) && fuelType == car.fuelType;
+        return modelYear == car.modelYear && Double.compare(emissions, car.emissions) == 0 && price == car.price && Objects.equals(id, car.id) && Objects.equals(carModel, car.carModel) && Objects.equals(manufacturer, car.manufacturer) && fuelType == car.fuelType;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(carModel, manufacturer, modelYear, fuelType, emissions, price);
+        return Objects.hash(id, carModel, manufacturer, modelYear, fuelType, emissions, price);
     }
 
-    // toString() method
     @Override
     public String toString() {
-        return "Make: " + manufacturer + "\n" +
-                "Model: " + carModel + "\n" +
-                "Fuel type: " + fuelType + "\n" +
-                "Emission: " + emissions + "\n" +
-                "Price: €" + price;
+        return "Car{" +
+                "id=" + id +
+                ", carModel='" + carModel + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", modelYear=" + modelYear +
+                ", fuelType=" + fuelType +
+                ", emissions=" + emissions +
+                ", price=" + price +
+                '}';
     }
+
 }
---
+++ src/main/java/ee/bcs/carportal/repository/CarRepositoryImpl.java
@@ -0,0 +1,50 @@
+package ee.bcs.carportal.repository;
+
+import ee.bcs.carportal.persistence.Car;
+import ee.bcs.carportal.persistence.FuelType;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class CarRepositoryImpl implements CarRepository {
+
+    private Map<Integer, Car> cars = createCars();
+
+    @Override
+    public void save(Car car) {
+        cars.put(car.getId(), car);
+    }
+
+    @Override
+    public Car getById(int id) {
+        return cars.get(id);
+    }
+
+    @Override
+    public List<Car> findAll() {
+        return new ArrayList<>(cars.values());
+    }
+
+    @Override
+    public void deleteById(int id) {
+        cars.remove(id);
+    }
+
+    public void resetData() {
+        cars = createCars();
+    }
+
+    private Map<Integer, Car> createCars() {
+        Map<Integer, Car> initialCars = new HashMap<>();
+        initialCars.put(1, new Car(1, "Model 3", "Tesla", 2020, FuelType.ELECTRIC, 0.0, 44000));
+        initialCars.put(2, new Car(2, "Civic", "Honda", 2021, FuelType.PETROL, 0.05, 25000));
+        initialCars.put(3, new Car(3, "Camry", "Toyota", 2022, FuelType.PETROL, 0.04, 28000));
+        initialCars.put(4, new Car(4, "F-150", "Ford", 2023, FuelType.PETROL, 0.1, 45000));
+        initialCars.put(5, new Car(5, "Prius", "Toyota", 2020, FuelType.HYBRID, 0.03, 30000));
+        return initialCars;
+    }
+}
