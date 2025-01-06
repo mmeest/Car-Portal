db5297dd714a7ef1ff8157bd708f37a8176f5faf
Rain Tüür <rain.tyyr@bcs.ee>
2025-01-06 10:14:59 +0200

    sprint 6 CP-31

---
+++ src/main/java/ee/bcs/carportal/repository/CarRepository.java
@@ -0,0 +1,18 @@
+package ee.bcs.carportal.repository;
+
+import ee.bcs.carportal.persistence.Car;
+
+import java.util.List;
+
+public interface CarRepository {
+
+    void save(Car car);
+
+    Car getById(int id);
+
+    List<Car> findAll();
+
+    void deleteById(int id);
+
+}
+
