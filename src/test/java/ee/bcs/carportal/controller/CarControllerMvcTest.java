5856b2d40129643fb02619b968d3a663ee5abd70
Rain Tüür <rain.tyyr@bcs.ee>
2025-02-17 08:33:28 +0200

    sprint 7 tests

--- src/test/java/ee/bcs/carportal/controller/CarControllerMvcTest.java
+++ src/test/java/ee/bcs/carportal/controller/CarControllerMvcTest.java
@@ -1,12 +1,10 @@
 package ee.bcs.carportal.controller;
 
-
-import ee.bcs.carportal.repository.CarRepositoryImpl;
-import jakarta.annotation.Resource;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.ResourceUtils;
 
@@ -14,31 +12,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class CarControllerMvcTest {
 
-    @Resource
-    private CarRepositoryImpl carRepository;  // Inject the real service
-
-    @Resource
+    @Autowired
     private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp() {
-        // Reset the cars data
-        carRepository.resetData();
-    }
 
     @Test
     public void shouldReturnAllCars() throws Exception {
         // Load the expected JSON from the file
-        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars.json").toURI());
+        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_all_cars.json").toURI());
         String expectedJson = Files.readString(path);
 
         // Assert: Perform the GET request and check the response
@@ -51,12 +41,12 @@ class CarControllerMvcTest {
     @Test
     public void shouldReturnCarsInPriceRange() throws Exception {
         // Load the expected JSON from the file
-        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars_price_range.json").toURI());
+        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars_price_range_30000_to_45000.json").toURI());
         String expectedJson = Files.readString(path);
 
         // Assert: Perform the GET request and check the response
         mockMvc.perform(get("/api/v1/cars/price-range")
-                        .param("from", "28000")
+                        .param("from", "30000")
                         .param("to", "45000"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
@@ -64,184 +54,60 @@ class CarControllerMvcTest {
     }
 
     @Test
-    public void shouldReturnGreenCarsInPriceRange() throws Exception {
+    public void shouldNotReturnAnyCarsInPriceRange() throws Exception {
+        // Assert: Perform the GET request and check the response
+        mockMvc.perform(get("/api/v1/cars/price-range")
+                        .param("from", "500")
+                        .param("to", "1000"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    public void shouldReturnCarsInPriceRange20000To25000WithFuelTypePetrol() throws Exception {
         // Load the expected JSON from the file
-        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_green_cars_price_range.json").toURI());
+        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars_price_range_with_fuel_type_20000_to_25000_P.json").toURI());
         String expectedJson = Files.readString(path);
 
         // Assert: Perform the GET request and check the response
-        mockMvc.perform(get("/api/v1/cars/green/price-range")
-                        .param("from", "28000")
-                        .param("to", "45000"))
+        mockMvc.perform(get("/api/v1/cars/price-range-fueltype")
+                        .param("from", "20000")
+                        .param("to", "25000")
+                        .param("fuelTypeCode", "P")
+                )
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(content().json(expectedJson));
     }
 
     @Test
-    public void shouldReturnRandomCarBasicInfo() throws Exception {
-        mockMvc.perform(get("/api/v1/car/random/basic-info"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(content().string(containsString("\"carModel\":")))
-                .andExpect(content().string(containsString("\"manufacturer\":")))
-                .andExpect(content().string(containsString("\"modelYear\":")))
-                .andExpect(content().string(containsString("\"fuelType\":")))
-                .andExpect(content().string(containsString("\"emissions\":")))
-                .andExpect(content().string(containsString("\"price\":")));
-    }
-
-    @Test
-    public void shouldReturnRandomCarDetailedInfo() throws Exception {
-        mockMvc.perform(get("/api/v1/car/random/detailed-info"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(content().string(containsString("\"carModel\":")))
-                .andExpect(content().string(containsString("\"manufacturer\":")))
-                .andExpect(content().string(containsString("\"modelYear\":")))
-                .andExpect(content().string(containsString("\"fuelType\":")))
-                .andExpect(content().string(containsString("\"emissions\":")))
-                .andExpect(content().string(containsString("\"price\":")));
-    }
-
-    @Test
-    public void shouldReturnCarBasicInfoById() throws Exception {
+    public void shouldReturnCarsInPriceRange20000To30000WithFuelTypePetrol() throws Exception {
         // Load the expected JSON from the file
-        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_car_basic_info_by_id.json").toURI());
+        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars_price_range_with_fuel_type_20000_to_30000_P.json").toURI());
         String expectedJson = Files.readString(path);
 
         // Assert: Perform the GET request and check the response
-        mockMvc.perform(get("/api/v1/car/1/basic-info"))
+        mockMvc.perform(get("/api/v1/cars/price-range-fueltype")
+                        .param("from", "20000")
+                        .param("to", "30000")
+                        .param("fuelTypeCode", "P")
+                )
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(content().json(expectedJson));
     }
 
     @Test
-    public void shouldReturnCarDetailedInfoById() throws Exception {
-        // Load the expected JSON from the file
-        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_car_detailed_info_by_id.json").toURI());
-        String expectedJson = Files.readString(path);
-
+    public void shouldNotReturnAnyCarsInPriceRangeWithFuelType() throws Exception {
         // Assert: Perform the GET request and check the response
-        mockMvc.perform(get("/api/v1/car/1/detailed-info"))
+        mockMvc.perform(get("/api/v1/cars/price-range-fueltype")
+                        .param("from", "20000")
+                        .param("to", "25000")
+                        .param("fuelTypeCode", "H"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
-                .andExpect(content().json(expectedJson));
+                .andExpect(content().json("[]"));
     }
 
-    @Test
-    public void shouldReturnCarsByRegistrationTaxRange() throws Exception {
-        // Load the expected JSON from the file
-        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars_registration_tax_range.json").toURI());
-        String expectedJson = Files.readString(path);
-
-        // Assert: Perform the GET request and check the response
-        mockMvc.perform(get("/api/v1/tax/cars/registration-tax-range")
-                        .param("from", "1100")
-                        .param("to", "1200")
-                        .param("baseYear", "2025"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(content().json(expectedJson));
-    }
-
-    @Test
-    public void shouldReturnCarsByAnnualTaxRange() throws Exception {
-        // Load the expected JSON from the file
-        Path path = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars_annual_tax_range.json").toURI());
-        String expectedJson = Files.readString(path);
-
-        // Assert: Perform the GET request and check the response
-        mockMvc.perform(get("/api/v1/tax/cars/annual-tax-range")
-                        .param("from", "50")
-                        .param("to", "126")
-                        .param("baseYear", "2025"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(content().json(expectedJson));
-    }
-
-
-    // new tests
-    @Test
-    public void shouldAddNewCar() throws Exception {
-        // Load the request JSON from the file
-        Path requestPath = Paths.get(ResourceUtils.getFile("classpath:files/request_add_car.json").toURI());
-        String requestJson = Files.readString(requestPath);
-
-        // Perform the POST request to add a new car
-        mockMvc.perform(post("/api/v1/car")
-                        .contentType("application/json")
-                        .content(requestJson))
-                .andExpect(status().isOk());
-
-        // Load the expected JSON from the file
-        Path expectedPath = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars_after_add.json").toURI());
-        String expectedJson = Files.readString(expectedPath);
-
-        // Assert that the car list is updated
-        mockMvc.perform(get("/api/v1/cars/all"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(content().json(expectedJson));
-    }
-
-    @Test
-    public void shouldUpdateCar() throws Exception {
-        // Load the request JSON from the file
-        Path requestPath = Paths.get(ResourceUtils.getFile("classpath:files/request_update_car.json").toURI());
-        String requestJson = Files.readString(requestPath);
-
-        // Perform the PUT request to update the car
-        mockMvc.perform(put("/api/v1/car/4")
-                        .contentType("application/json")
-                        .content(requestJson))
-                .andExpect(status().isOk());
-
-        // Load the expected JSON from the file
-        Path expectedPath = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars_after_update.json").toURI());
-        String expectedJson = Files.readString(expectedPath);
-
-        // Assert that the car list is updated
-        mockMvc.perform(get("/api/v1/cars/all"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(content().json(expectedJson));
-    }
-
-    @Test
-    public void shouldUpdateCarPrice() throws Exception {
-        // Perform the PATCH request to update the car price
-        mockMvc.perform(patch("/api/v1/car/3")
-                        .param("price", "30000"))
-                .andExpect(status().isOk());
-
-        // Load the expected JSON from the file
-        Path expectedPath = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars_after_price_update.json").toURI());
-        String expectedJson = Files.readString(expectedPath);
-
-        // Assert that the car list is updated
-        mockMvc.perform(get("/api/v1/cars/all"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(content().json(expectedJson));
-    }
-
-    @Test
-    public void shouldDeleteCar() throws Exception {
-        // Perform the DELETE request to delete a car
-        mockMvc.perform(delete("/api/v1/car/3"))
-                .andExpect(status().isOk());
-
-        // Load the expected JSON from the file
-        Path expectedPath = Paths.get(ResourceUtils.getFile("classpath:files/expected_cars_after_delete.json").toURI());
-        String expectedJson = Files.readString(expectedPath);
-
-        // Assert that the car list is updated
-        mockMvc.perform(get("/api/v1/cars/all"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(content().json(expectedJson));
-    }
 }
