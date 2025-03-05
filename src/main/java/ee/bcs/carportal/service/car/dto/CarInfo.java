bc161853188921650d0c5f802fe7f3cad5ce21bf
mmeest <martin.maasik.001@mail.ee>
2025-03-05 21:11:51 +0200

    sprint-7-MARTIN-MAASIK CP-47 complete

---
+++ src/main/java/ee/bcs/carportal/service/car/dto/CarInfo.java
@@ -0,0 +1,14 @@
+package ee.bcs.carportal.service.car.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarInfo {
+    String make;
+    String modelName;
+    Integer releaseYear;
+}
